e46f4fc160622e2009aa0e3ef7ed3c7f5f4f4262
Shaun Daly <37176994+G00286445@users.noreply.github.com>
2018-12-17 14:48:07 +0000

    Add files via upload

---
+++ ProjectV1/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ProjectV1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        // Populates table with data from SQLite
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            using (SQLite.SQLiteConnection conn = new SQLite.SQLiteConnection(App.DB_PATH))
+            {
+                conn.CreateTable<Player>();
+
+                var players = conn.Table<Player>().ToList();
+                playersListView.ItemsSource = players;
+            }
+        }
+
+        // Navigation for toolbar on top of MainPage
+        private void ToolbarItem_Activated(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new NewPlayerPage());
+        }
+
+        // Remove a player from list
+        private void ToolbarItem_Activated_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ProjectV1/Player.cs
@@ -0,0 +1,17 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectV1
+{
+    public class Player
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        
+        public string Name { get; set; }
+
+        public string Club { get; set; }
+    }
+}
